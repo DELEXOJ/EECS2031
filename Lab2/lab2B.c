d919174504426596e7c4fd889ef538949db2ee56
Ayobami Esther Ojo <36674978+DELEXOJ@users.noreply.github.com>
2022-02-07 08:18:43 -0500

    Removed my personal info

--- Lab2/lab2B.c
+++ Lab2/lab2B.c
@@ -1,9 +1,6 @@
 /***************************************
 * EECS2031ON – Lab2 *
-* Author: OJO, AYOBAMI *
-* Email:ayobami1@my.yorku.ca *
-* eecs_username: ayobami1 *
-* York Student #: 215281421
+* A
 ****************************************/
 
 #include <stdio.h>
