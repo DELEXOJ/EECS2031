c978c309e4d123d0a4d50fc6dd045a3664e29f8a
Ayobami Esther Ojo <36674978+DELEXOJ@users.noreply.github.com>
2022-02-07 08:07:32 -0500

    Removed my personal info

--- Lab6/lab6LL1.c
+++ Lab6/lab6LL1.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-/***************************************
- * EECS2031ON – Lab6 *
- * Author: OJO, AYOBAMI*
- * Email: ayobami1@my.yorku.ca *
- * eecs_num: ayobami1*
- * Yorku #: 215281421
+/********************
  ****************************************/
 
 void insertBegining(int);
